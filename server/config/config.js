ad274d40c88891b096fe7caaf3b69cfbaad10c95
Jaysig <jasontsigmon@gmail.com>
2016-01-13 22:38:02 -0500

    commit test

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  'url': 'mongodb://localhost/lolChampSelect'
-}
+  'url': 'mongodb://localhost/lolChampSelect',
+  'leagueAPIKey': 'd9385b48-9aaf-4767-a70e-9f9fdf777451'
+};
---
+++ server/server.js
@@ -0,0 +1,14 @@
+var LolApi = require('leagueapi'),config = require('/config/config.js');
+
+
+LolApi.init(leagueAPIKey, 'na');
+
+LolApi.getChampions(true, function(err, champs) {
+    console.log(champs);
+});
+
+LolApi.Summoner.getByName('YOLO Swag 5ever', function(err, summoner) {
+    if(!err) {
+        console.log(summoner);
+    }
+})
