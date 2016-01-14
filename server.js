e80718feab9ddb0e3928b56f93ee556bcc44ca22
Jaysig <jasontsigmon@gmail.com>
2016-01-13 23:14:09 -0500

    server has access to league API
    able to use the LoL api

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var config = require('./server/config/config');
 var mongoose = require('mongoose');
+var LolApi = require('leagueapi');
 
 // Connect to DB
 if (process.env.MONGOLAB_URI) {
@@ -19,6 +20,19 @@ connectDB.once('open', function() {
 
 
 
+
+LolApi.init(config.leagueAPIKey, 'na');
+
+LolApi.getChampions(true, function(err, champs) {
+    console.log(champs);
+});
+
+// Method list for the LolApi Wrapper
+// https://github.com/claudiowilson/LeagueJS
+options = {champData: 'allytips,image,recommended', version : '4.4.3', locale: 'en_US', dataById: true};
+LolApi.Static.getChampionList(options, function(err, champs) {
+    console.log(champs,' champs');
+});
 var app = express();
 
 require('./server/config/middleware.js')(app, express);
